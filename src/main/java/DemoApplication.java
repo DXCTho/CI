e4c851eca2ed1179feda16add8b385e6d7c09127
Thorsten <50742079+DXCTho@users.noreply.github.com>
2019-07-12 08:32:03 +0200

    Update Code - CodeSmellMethod

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -9,4 +9,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+    private void johnsUnusedCodeSmellMethod() {
+       String buggy=null;
+        buggy.trim();
+        
+        System.out.println("Foo!");
+        
+}
 }
